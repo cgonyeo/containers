1876659e6327dd95b2414e23ba102383f56332c2
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-10-01 10:57:12 -0700

    Updated plex's build script to use new acbuild syntax

--- rkt/plex/build-plex.sh
+++ rkt/plex/build-plex.sh
@@ -1,34 +1,34 @@
 # Begin the build
-acbuild --debug init
+acbuild begin
 
 # Name the aci
-acbuild --debug name aci.gonyeo.com/plex
+acbuild name aci.gonyeo.com/plex
 
-acbuild add-dep aci.gonyeo.com/ubuntu
+acbuild dependency add aci.gonyeo.com/ubuntu
 
 # Download and install plex
-acbuild --debug exec -- apt-get update
-acbuild --debug exec -- apt-get install -y wget
-acbuild --debug exec -- wget https://downloads.plex.tv/plex-media-server/0.9.12.11.1406-8403350/plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
-acbuild --debug exec -- dpkg -i plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
-acbuild --debug exec -- rm plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
-acbuild --debug exec -- rm -f ./rootfs/var/cache/apt/archives/*.deb ./rootfs/var/cache/apt/archives/partial/*.deb ./rootfs/var/cache/apt/*.bin
+acbuild exec -- apt-get update
+acbuild exec -- apt-get install -y wget
+acbuild exec -- wget https://downloads.plex.tv/plex-media-server/0.9.12.11.1406-8403350/plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
+acbuild exec -- dpkg -i plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
+acbuild exec -- rm plexmediaserver_0.9.12.11.1406-8403350_amd64.deb
+acbuild exec -- rm -f ./rootfs/var/cache/apt/archives/*.deb ./rootfs/var/cache/apt/archives/partial/*.deb ./rootfs/var/cache/apt/*.bin
 
 # Set environment variables for plex
-acbuild --debug add-env PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR "/var/lib/plexmediaserver/Library/Application Support"
-acbuild --debug add-env PLEX_MEDIA_SERVER_HOME /usr/lib/plexmediaserver
-acbuild --debug add-env PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS 6
-acbuild --debug add-env PLEX_MEDIA_SERVER_TMPDIR /tmp
-acbuild --debug add-env LD_LIBRARY_PATH /usr/lib/plexmediaserver
-acbuild --debug add-env LC_ALL en_US.UTF-8
-acbuild --debug add-env LANG en_US.UTF-8
+acbuild environment add PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR "/var/lib/plexmediaserver/Library/Application Support"
+acbuild environment add PLEX_MEDIA_SERVER_HOME /usr/lib/plexmediaserver
+acbuild environment add PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS 6
+acbuild environment add PLEX_MEDIA_SERVER_TMPDIR /tmp
+acbuild environment add LD_LIBRARY_PATH /usr/lib/plexmediaserver
+acbuild environment add LC_ALL en_US.UTF-8
+acbuild environment add LANG en_US.UTF-8
 
 # Set plex to be the run command
-acbuild --debug set-run "/usr/lib/plexmediaserver/Plex Media Server"
+acbuild set-run "/usr/lib/plexmediaserver/Plex Media Server"
 
 # Add mount points for configs and media
-acbuild --debug add-mount config "/var/lib/plexmediaserver/Library/Application Support"
-acbuild --debug add-mount media /media --read-only
+acbuild mount add config "/var/lib/plexmediaserver/Library/Application Support"
+acbuild mount add media /media --read-only
 
 # Finish the build
-acbuild --debug finish --overwrite plex.aci
+acbuild end --overwrite plex.aci
