496034f800dc1ac8fa41b59227dfa6cc36212aac
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-10-13 11:28:20 -0700

    Use bash traps to abort intelligently in the blog build script

--- rkt/blog/build-site.sh
+++ rkt/blog/build-site.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+set -e
+
 acbuild --debug begin
+
+trap "acbuild --debug abort" EXIT
+
 acbuild --debug dep add aci.gonyeo.com/nginx
 acbuild --debug copy git/blog/_site /usr/html
 acbuild --debug port add http tcp 80
 acbuild --debug set-name "aci.gonyeo.com/blog"
 acbuild --debug set-exec -- "/usr/sbin/nginx" "-g" "daemon off;"
 acbuild --debug end --sign --user derek blog.aci
+
+trap - EXIT
