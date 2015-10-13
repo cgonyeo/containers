69401e3a99f87a2b2c4bced4ed81001ee0360368
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-10-13 13:29:56 -0700

    Switched to use the write command

--- rkt/blog/build-site.sh
+++ rkt/blog/build-site.sh
@@ -3,13 +3,11 @@ set -e
 
 acbuild --debug begin
 
-trap "{ acbuild --debug abort; exit 1; }" EXIT
+trap "{ export EXT=$?; acbuild --debug end; exit $EXT; }" EXIT
 
 acbuild --debug dep add aci.gonyeo.com/nginx
 acbuild --debug copy git/blog/_site /usr/html
 acbuild --debug port add http tcp 80
 acbuild --debug set-name "aci.gonyeo.com/blog"
 acbuild --debug set-exec -- "/usr/sbin/nginx" "-g" "daemon off;"
-acbuild --debug end --sign --user derek blog.aci
-
-trap - EXIT
+acbuild --debug write --sign --user derek blog.aci
