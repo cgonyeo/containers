c4f085d8c411caf6a4e41896b533885266d00c78
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-10-13 14:56:02 -0700

    more acbuild tweaks

--- rkt/blog/build-site.sh
+++ rkt/blog/build-site.sh
@@ -6,8 +6,8 @@ acbuild --debug begin
 trap "{ export EXT=$?; acbuild --debug end; exit $EXT; }" EXIT
 
 acbuild --debug dep add aci.gonyeo.com/nginx
-acbuild --debug copy git/blog/_site /usr/html
+acbuild --debug copy _site /usr/html
 acbuild --debug port add http tcp 80
 acbuild --debug set-name "aci.gonyeo.com/blog"
 acbuild --debug set-exec -- "/usr/sbin/nginx" "-g" "daemon off;"
-acbuild --debug write --sign --user derek blog.aci
+acbuild --debug write --sign blog.aci
