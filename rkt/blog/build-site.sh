9107350e006ab94fea5a561922ee8a5fe5f15d1e
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-10-13 11:00:14 -0700

    Updated blog to have own container, with nginx dep

---
+++ rkt/blog/build-site.sh
@@ -0,0 +1,7 @@
+acbuild --debug begin
+acbuild --debug dep add aci.gonyeo.com/nginx
+acbuild --debug copy git/blog/_site /usr/html
+acbuild --debug port add http tcp 80
+acbuild --debug set-name "aci.gonyeo.com/blog"
+acbuild --debug set-exec -- "/usr/sbin/nginx" "-g" "daemon off;"
+acbuild --debug end --sign --user derek blog.aci
