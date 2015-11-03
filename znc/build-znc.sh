4df198e3d4bb63ca299d25b34ddff5c4b0cf0306
Derek Gonyeo <derek@gonyeo.com>
2015-11-03 15:08:13 -0800

    tweaks to znc build script. nonfunctional.

--- znc/build-znc.sh
+++ znc/build-znc.sh
@@ -6,33 +6,34 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+acbuildend () { export EXT=$?; acbuild --debug end && exit $EXT; }
+
 # Start the build with an empty ACI
 acbuild --debug begin
 
 # In the event of the script exiting, end the build
-trap "{ export EXT=$?; acbuild --debug end && exit $EXT; }" EXIT
+trap acbuildend EXIT
 
 # Name the ACI
 acbuild --debug set-name aci.gonyeo.com/znc
 
 # Based on alpine
-acbuild --debug dep add quay.io/coreos/alpine-sh
+acbuild --debug dep add aci.gonyeo.com/alpine
 
 # Install znc
-acbuild --debug run apk update
-acbuild --debug run apk add znc
-acbuild --debug run mkdir /home/znc
-acbuild --debug run chown znc:znc /home/znc
+acbuild --debug run -- apk update
+acbuild --debug run -- apk add znc
+acbuild --debug run -- adduser -h /home/zncuser -D -u 1111 zncuser
 
 # Run znc in the foreground
 acbuild --debug set-exec -- /usr/bin/znc --foreground
 
 # Add a new user, set the app to run as the new user
-acbuild --debug set-user 1000
-acbuild --debug set-group 1000
+acbuild --debug set-user 1111
+acbuild --debug set-group 1111
 
 # Add a mount point for znc's config
-acbuild --debug mount add configs /usr/share/nginx/html
+acbuild --debug mount add configs /home/zncuser/.znc
 
 # Save the ACI
 acbuild --debug write --overwrite znc-latest-linux-amd64.aci
