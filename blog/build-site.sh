bebf4dded111b2618df5d69adf464c1bbba57b9d
Derek Gonyeo <derek.gonyeo@coreos.com>
2016-01-11 18:36:48 -0500

    don't sign, use passed in path for html source, comments

--- blog/build-site.sh
+++ blog/build-site.sh
@@ -1,13 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Exit if an error occurs
 set -e
 
+# Function to call for cleanup
+acbuildend () { export EXT=$?; acbuild --debug end && exit $EXT; }
+
+# Begin a new build
 acbuild --debug begin
 
-trap "{ export EXT=$?; acbuild --debug end && exit $EXT; }" EXIT
+# When the script exits, call cleanup
+trap acbuildend EXIT
 
+# Add a dependency on the nginx image
 acbuild --debug dep add aci.gonyeo.com/nginx
-acbuild --debug copy _site /usr/html
+
+# Copy the site to nginx's serve location
+acbuild --debug copy $1 /usr/share/nginx/html
+
+# Add a port for the site to be hosted over
 acbuild --debug port add http tcp 80
+
+# Set the name of the image
 acbuild --debug set-name "aci.gonyeo.com/blog"
+
+# Exec nginx in the foreground
 acbuild --debug set-exec -- "/usr/sbin/nginx" "-g" "daemon off;"
-acbuild --debug write --sign blog.aci
+
+# Write the image
+acbuild --debug write blog-latest-linux-amd64.aci
